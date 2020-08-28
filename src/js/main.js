d38feb66bb797ee3c4b4817bb326ebae44c2e2e4
SmartyAalexxx <smarty.mazny@gmail.com>
2020-08-28 18:29:10 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+//= lib/jquery.js
+///= lib/popper.js
+///= lib/jQuery.headroom.js
+///= lib/headroom.js
+///= lib/slick.js
+///= lib/isotope.js
+
+//= bootstrap/util.js
+
+///= bootstrap/modal.js
+///= bootstrap/alert.js
+///= bootstrap/button.js
+///= bootstrap/collapse.js
+///= bootstrap/carousel.js
+///= bootstrap/dropdown.js
+///= bootstrap/index.js
+///= bootstrap/popover.js
+///= bootstrap/scrollspy.js
+///= bootstrap/tab.js
+///= bootstrap/toast.js
+///= bootstrap/tooltip.js
+///= bootstrap/tools/sanitizer.js
+
+//= scripts.js
